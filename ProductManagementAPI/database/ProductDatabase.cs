fb6fc630615ff99052cef69d3de05d0d8e275379
amina12-aj <bakareaminata@gmail.com>
2022-07-23 17:59:51 +0100

    main

---
+++ ProductManagementAPI/database/ProductDatabase.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductManagementAPI.database
+{
+    public class ProductDatabase: DbContext
+    {
+    }
+}
--- ProductManagementAPI/model/Category.cs
+++ ProductManagementAPI/model/Category.cs
@@ -1,18 +1,31 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProductManagementAPI.model
 {
-    public static class Category
+    public class Category
     {
         [Key]
-        public int id { get; set; }
+        public int Id { get; set; }
         [Required]
-        public string productName { get; set; }
-        public string price { get; set; }
-        public string productType{ get; set; }
-        public DateTime date { get; set; }
-    }
+        public string? ProductName { get; set; }
+        [Required]
+        public string? Price { get; set; }
+        public string? ProductType { get; set; }
+        [Required]
+        public DateTime? Date { get; set; }
 
-    
+        //creating a list that holds all the items
+        List<Products> ProductList = new List<Products>
+        { new ProductList { Id = 1, ProductName = "Eggs", Price = 123456, ProductType = "Consumer", DateTime = },
+            new ProductList { Id = 2, ProductName = "Rice", Price = 1000, ProductType = "Consumer", DateTime = },
+            new ProductList { Id = 3, ProductName = "Stove", Price = 600, ProductType = "Electronics", DateTime = },
+            new ProductList { Id = 4, ProductName = "Blender", Price = 12000, ProductType = "Electronics", DateTime = },
+            new ProductList { Id = 5, ProductName = "Beans", Price = 500, ProductType = "Consumer", DateTime = },
+            new ProductList { Id = 6, ProductName = "CornFlakes", Price = 1200, ProductType = "Consumer", DateTime = } 
+        };
+
+
+            
 }
  
