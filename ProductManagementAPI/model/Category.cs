442d10cff66935e499d89e0f16d60597eb6a6ae6
amina12-aj <bakareaminata@gmail.com>
2022-07-23 15:27:23 +0100

    main

---
+++ ProductManagementAPI/model/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductManagementAPI.model
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string productName { get; set; }
+        public string price { get; set; }
+        public string productType{ get; set; }
+        public DateTime date { get; set; }
+    }
+}
+
